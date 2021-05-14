9aafb7cd30933737766432dfc3fb09882b59acee
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-05-14 13:49:39 -0300

    💄 Adding more content of the films in the cards

--- src/components/FilmesList/FilmesList.jsx
+++ src/components/FilmesList/FilmesList.jsx
@@ -1,4 +1,5 @@
 import { Card, makeStyles } from "@material-ui/core";
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { useState } from "react";
 
 const useStyles = makeStyles(theme => ({
@@ -8,18 +9,38 @@ const useStyles = makeStyles(theme => ({
         justifyContent: 'center',
     },
     card: {
-        padding: '10px',
         margin: '10px',
-        width: '15%',
         background: 'skyblue',
         borderRadius: '5px',
         boxShadow: '2px 1px 3px',
         alignItems: 'center',
         display: 'flex',
-        justifyContent: 'center',
         minHeight: '80px'
+    },
+    info: {
+        width: '130px',
+        padding: '0 10px',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    nota: {
+        display: 'inline-flex',
+        alignItems: 'center'
     }
 }))
+
+const trataData = (data) => {
+    const meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+    const mes = meses[new Date(data).getMonth()];
+
+    data = new Date(data).toLocaleDateString();
+    data = data.replace(/(\/\w{2})/, '/'+mes);
+    data = data.replaceAll('/', ' de ');
+
+    return data;
+}
 
 const FilmesList = () => {
     const classes = useStyles();
@@ -35,11 +56,24 @@ const FilmesList = () => {
 
     return (
         <div className={classes.cardContainer}>
-            {filmes.results && filmes.results.map( filme =>
-                <Card key={filme.id} className={classes.card}>
-                    {filme.title || filme.name}
+            {filmes.results && filmes.results.map( filme => {
+                const {title, name, release_date, first_air_date, id, poster_path, vote_average} = filme;
+                let data = trataData(release_date || first_air_date)
+
+                return <Card key={id} className={classes.card}>
+                    <img width="150" src={`https://www.themoviedb.org/t/p/w220_and_h330_bestv2/${poster_path}`} alt="poster" />
+                    <div className={classes.info}>
+                        <p>{title || name}</p>
+                        <div>
+                            <p>{data}</p>
+                            <p className={classes.nota}>
+                                {vote_average > 6 && <StarBorderIcon color="secondary" />}
+                                <span>Nota: {vote_average}</span>
+                            </p>
+                        </div>
+                    </div>
                 </Card>
-            )}
+            })}
         </div>
     )
 }
